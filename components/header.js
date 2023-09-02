d41cfe519f313edddad16091a2d98f8531c5cd37
stogashi146 <stogashi146@gmail.com>
2023-09-02 15:03:01 +0900

    footerのCSS実装

--- components/header.js
+++ components/header.js
@@ -1,12 +1,15 @@
 import Logo from "./logo";
 import Nav from "./nav";
+import styles from "/styles/header.module.css";
 
 export default function Header() {
   return (
     <>
       <header>
-        <Logo boxOn />
-        <Nav />
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
       </header>
     </>
   );
